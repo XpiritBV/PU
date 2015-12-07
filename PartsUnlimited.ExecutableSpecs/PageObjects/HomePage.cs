74aadf01c63c440e47f5d07380dc3931e449a937
Jesse Houwing <jesse.houwing@gmail.com>
2015-12-07 18:32:13 +0100

    Places SearchResultsPage in its own file

--- PartsUnlimited.ExecutableSpecs/PageObjects/HomePage.cs
+++ PartsUnlimited.ExecutableSpecs/PageObjects/HomePage.cs
@@ -9,7 +9,7 @@ namespace PartsUnlimited.ExecutableSpecs.PageObjects
             this.Url = "http://localhost:5001/";
             this.At = () => this.I.Expect.Exists(SearchInput);
         }
-
+         
         public HomePage EnterSearchCriteria(string searchText)
         {
             this.I.Enter(searchText).In(SearchInput);
@@ -24,24 +24,5 @@ namespace PartsUnlimited.ExecutableSpecs.PageObjects
 
 
         private const string SearchInput = "#search-box";
-    }
-
-    public class SearchResultsPage : PageObject<SearchResultsPage>
-    {
-        public SearchResultsPage(FluentTest test)
-            : base(test)
-        {
-            this.Url = "http://localhost:5001/Search";
-            this.At = () => this.I.Expect.Exists(SearchResultsContainer);
-        }
-
-        public SearchResultsPage FindResultByTitle(string title)
-        {
-            this.I.Expect.Exists(string.Format(ResultTitle, title));
-            return this;
-        }
-
-        private const string SearchResultsContainer = "#search-page";
-        private const string ResultTitle = "#search-page > .list-item-part > h4:contains('{0}')";
     }
 }
---
+++ PartsUnlimited.ExecutableSpecs/PageObjects/SearchResultsPage.cs
@@ -0,0 +1,23 @@
+﻿using FluentAutomation;
+
+namespace PartsUnlimited.ExecutableSpecs.PageObjects
+{
+    public class SearchResultsPage : PageObject<SearchResultsPage>
+    {
+        public SearchResultsPage(FluentTest test)
+            : base(test)
+        {
+            this.Url = "http://localhost:5001/Search";
+            this.At = () => this.I.Expect.Exists(SearchResultsContainer);
+        }
+
+        public SearchResultsPage FindResultByTitle(string title)
+        {
+            this.I.Expect.Exists(string.Format(ResultTitle, title));
+            return this;
+        }
+
+        private const string SearchResultsContainer = "#search-page";
+        private const string ResultTitle = "#search-page > .list-item-part > h4:contains('{0}')";
+    }
+}
