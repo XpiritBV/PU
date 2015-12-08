2eb8701c1d694b9149f456a35bd169ab3bf34ee4
Jesse Houwing <jesse.houwing@gmail.com>
2015-12-08 13:28:45 +0100

    Naive RecommendationEngine tests added

---
+++ PartsUnlimited.WebSite.UnitTests/Recommendations/NaiveRecommendationEngineTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using PartsUnlimited.Recommendations;
+
+using Xunit;
+
+namespace PartsUnlimited.WebSite.UnitTests.Recommendations
+{
+    public class NaiveRecommendationEngineTests
+    {
+        [Fact]
+        public void NaiveRecommendationEngineReturnsSameProductId()
+        {
+            IRecommendationEngine engine = new NaiveRecommendationEngine();
+
+            string productId = "MyProduct";
+
+            IEnumerable<string> recommendations = engine.GetRecommendationsAsync(productId).Result;
+
+            Assert.Equal(new[] { productId }, recommendations.ToArray());
+        }
+
+        [Fact]
+        public void NaiveRecommendationEngineReturnsNullArrayForNull()
+        {
+            IRecommendationEngine engine = new NaiveRecommendationEngine();
+
+            string productId = null;
+
+            IEnumerable<string> recommendations = engine.GetRecommendationsAsync(productId).Result;
+
+            Assert.Equal(new[] { productId }, recommendations.ToArray());
+        }
+    }
+}
