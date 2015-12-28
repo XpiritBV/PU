c8c05d5e829833c023f8216314e7d94bc695f744
rvanosnabrugge <rvanosnabrugge@live.com>
2015-12-28 17:45:57 +0100

    Deployment Scripts

---
+++ deploy/NewAzureConnectionString.ps1
@@ -0,0 +1,24 @@
+﻿#NewAzureConnectionString
+param (
+    [string]$webappname,
+	[string]$connectionStringName,
+	[string]$connectionStringValue
+)
+
+
+#https://gallery.technet.microsoft.com/scriptcenter/Deploy-a-Windows-Azure-Web-790cacd2
+#https://azure.microsoft.com/en-us/blog/windows-azure-web-sites-how-application-strings-and-connection-strings-work/
+
+#AzureConnectionString
+$appDBConnStrInfo = New-Object Microsoft.WindowsAzure.Commands.Utilities.Websites.Services.WebEntities.ConnStringInfo; 
+$appDBConnStrInfo.Name=$connectionStringName
+$appDBConnStrInfo.ConnectionString=$connectionStringValue; 
+$appDBConnStrInfo.Type="SQLAzure"; 
+
+
+# Add new ConnStringInfo objecto list of connection strings for website. 
+$connStrSettings = (Get-AzureWebsite $webappname).ConnectionStrings; 
+$connStrSettings.Add($appDBConnStrInfo); 
+
+Set-AzureWebsite -Name $webappname -ConnectionStrings $connStrSettings 
+
