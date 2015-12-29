f75c937e327509514bbf3af3812acb0708349e92
rvanosnabrugge <rvanosnabrugge@live.com>
2015-12-29 18:35:26 +0100

    Added simple Unit TEsts

---
+++ PartsUnlimited.WebSite.UnitTests/Recommendations/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PartsUnlimited.WebSite.UnitTests.Recommendations
+{
+    [TestClass]
+    public class ShoppingCartTests
+    {
+        [TestMethod]
+        public void ValidateShoppingCart()
+        {
+            Assert.AreEqual("1", "1");
+        }
+    }
+}
