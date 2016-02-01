5b6543c9d82482b29abbdffb602bad2b7dab6896
Marcel de Vries <Marcel@Partsunlimited.onmicrosoft.com>
2016-02-01 19:49:05 +0000

    added missing file

---
+++ Partsunlimited.PageObjectModel/PageObjects/CategoryPage.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UITesting.HtmlControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Partsunlimited.PageObjectModel.PageObjects
+{
+    public class CategoryPage
+    {
+        BrowserWindow _bw;
+        public CategoryPage(BrowserWindow bw)
+        {
+            _bw = bw;
+        }
+
+        public ProductDetailsPage SelectProduct(string productName)
+        {
+            HtmlDiv LightingList = new HtmlDiv(_bw);
+            LightingList.SearchProperties.Add(HtmlControl.PropertyNames.Class, "list-item-part", PropertyExpressionOperator.Contains);
+  
+            HtmlHyperlink lightingProduct = new HtmlHyperlink(LightingList);
+            lightingProduct.SearchProperties.Add(HtmlControl.PropertyNames.Title, productName, PropertyExpressionOperator.Contains);
+  
+            Mouse.Click(lightingProduct);
+
+            return new ProductDetailsPage(_bw);
+        }
+    }
+}
