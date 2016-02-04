0c53d37e5d3536c9b6bbdc856e838abf083ce3d4
Marcel de Vries <Marcel@Partsunlimited.onmicrosoft.com>
2016-02-04 18:58:46 +0000

    Updated SetSlotTrafic.ps1

--- env/PartsUnlimitedEnv/Scripts/SetSlotTrafic.ps1
+++ env/PartsUnlimitedEnv/Scripts/SetSlotTrafic.ps1
@@ -6,7 +6,7 @@ param (
 )
 Write-Host "Setting slot on webapp $webAppName slotname $canarySlotName to $percentage % trafic"
 $rule = New-Object Microsoft.WindowsAzure.Commands.Utilities.Websites.Services.WebEntities.RampUpRule
-$rule.ActionHostName = $canarySlotName
+$rule.ActionHostName = "$webAppName-$canarySlotName.azurewebsites.net"
 $rule.ReroutePercentage = $percentage
-$rule.Name = "canary-$percentage"
+$rule.Name = $canarySlotName
 Set-AzureWebsite $webAppName -Slot $productionSlotName -RoutingRules $rule
