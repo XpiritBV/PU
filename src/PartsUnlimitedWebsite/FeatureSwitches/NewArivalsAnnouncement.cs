c916a6b0ec663d8a89806ad7c0a61a140649ca3c
Marcel de Vries <Marcel@Partsunlimited.onmicrosoft.com>
2016-02-04 12:57:03 +0000

    added feature toggle for announcements

---
+++ src/PartsUnlimitedWebsite/FeatureSwitches/NewArivalsAnnouncement.cs
@@ -0,0 +1,21 @@
+﻿using FeatureSwitcher;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PartsUnlimited.FeatureSwitches
+{
+    public class NewArivalsAnnouncement : IFeature
+    {
+        public static string Name
+        {
+            get
+            {
+                return "New-Arivals";
+            }
+            internal
+                set { }
+        }
+    }
+}
